d46c4ab8a9f07024808e2602a228e2611494df56
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-14 20:11:53 +0530

    added UI to display chat message

--- public/script.js
+++ public/script.js
@@ -9,6 +9,18 @@ chat.addEventListener("submit", (event) => {
   input.value = "";
 });
 
+const chatwindow = document.getElementById("chatwindow");
+
+// function to show chat messeage at the frontend
+const renderMessage = (message) => {
+  const div = document.createElement("div");
+  div.classList.add("render-message");
+  div.innerText = message;
+  chatwindow.appendChild(div);
+};
+
 socket.on("chat", (message) => {
-  console.log("Form server:", message);
+  //console.log("Form server:", message);
+
+  renderMessage(message);
 });
