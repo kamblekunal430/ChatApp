05df831a1deccfc9d8a36a6bb2bfa3ef9da15e02
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-14 19:46:18 +0530

    added socket library and connected to the static html page

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const server = require("http").createServer(app);
 
 const port = process.env.PORT || 3344;
 
+const path = require("path");
+
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+io.on("connection", (socket) => {
+  console.log(`Connected with Socket [id: ${socket.id}]`);
+});
+
 // To check whether server is working fine.
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
